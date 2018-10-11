a6c5c60104498ae5e595e26aadc04bc41633b589
fpcasale <fncpaolo.casale@gmail.com>
2018-10-11 13:51:37 -0400

    fixed test

--- geno_sugar/test/test_preprocess.py
+++ geno_sugar/test/test_preprocess.py
@@ -1,9 +1,9 @@
 import numpy as np
-from geno_sugar.preprocess import standardize
+from geno_sugar.utils import standardize_snps
 
 
 def test_preprocess():
     random = np.random.RandomState(0)
     X = random.randn(3, 4)
-    np.testing.assert_allclose(standardize()(X).mean(axis=1), [0, 0, 0], atol=1e-7)
-    np.testing.assert_allclose(standardize()(X).std(axis=1), [1, 1, 1], atol=1e-7)
+    np.testing.assert_allclose(standardize_snps(X).mean(), [0, 0, 0], atol=1e-7)
+    np.testing.assert_allclose(standardize_snps(X).std(), [1, 1, 1], atol=1e-7)
